6c3f4a7a4772e02b033a0259e609a583c85e3a39
Shahnawaz <shahnawazkhan333@live.com>
2024-03-21 01:33:14 +0500

    Added my project

---
+++ App.js
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable eol-last */
+/* eslint-disable no-undef */
+/* eslint-disable prettier/prettier */
+
+import React from 'react';
+import AlarmClock from './AlarmClock';
+
+export default App = () => {
+	return (
+		<>
+			<AlarmClock />
+		</>
+	);
+};
